701da450aa280ed5fa01f0c2e24f9d2e476fe5e3
JunaidRehman2112 <junaidurrehman2112@gmail.com>
2024-05-03 09:49:08 +0500

    complete delete user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -119,6 +119,22 @@ export default function Profile() {
       setShowListingsError(true)
     }
   }
+  const handleListingDelete = async (lisitngId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${lisitngId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+       if (data.success === false) {
+        console.log(data.message)
+        return
+       }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== lisitngId))       
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
     <div className='max-w-lg p-3 mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -212,7 +228,7 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link> 
               <div className="flex flex-col items-center">
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
